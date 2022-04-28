75c397dcb8462d1497a110e0b7494022aa52b3ff
gyugyu1 <lwd33021@naver.com>
2022-04-28 09:22:42 +0900

    수빈님이 만든 베이스 템플렛에서 네비바, 사이드바 따로 추출

--- src/main/java/com/cacao/classting/member/MemberController.java
+++ src/main/java/com/cacao/classting/member/MemberController.java
@@ -1,2 +1,53 @@
 package com.cacao.classting.member;
-/* example. */
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(Locale locale, Model model) {
+				
+		return "member/include/classBase";
+	}
+	@RequestMapping(value = "/basetemplate", method = RequestMethod.GET)
+	public String baseTemplate() {
+		
+		return "member/include/classBaseTemplate";
+		
+	}
+	@RequestMapping(value = "/include", method = RequestMethod.GET)
+	public String include() {
+		
+		return "member/include/baseTest";
+	}
+	
+	@RequestMapping(value = "/side", method = RequestMethod.GET)
+	public String side() {
+		
+		return "member/include/sideBar";
+	}
+	
+	@RequestMapping(value = "/nav", method = RequestMethod.GET)
+	public String nav() {
+		
+		return "member/include/navBar";
+	}
+}
