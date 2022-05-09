41c12d0c8f2fa5fb765ac86f9f15491f8cd823b0
dbstnqls12 <binson1123@naver.com>
2022-05-09 11:55:23 +0900

    chat, classroom, member dto 입력/ db모델링 수정

--- src/main/java/com/cacao/classting/member/Member.java
+++ src/main/java/com/cacao/classting/member/Member.java
@@ -1,30 +1,175 @@
 package com.cacao.classting.member;
 
+import java.util.Date;
+
 public class Member {
 
-	private String ifmmSeq = "";
-	private String ifmmId = "";
-
-
-	// ------------
-
-	public String getIfmmSeq() {
-		return ifmmSeq;
+//	Member
+	private String mmSeq;
+	private String mmName;
+	private String mmId;
+	private String mmPassword;
+	private String mmMemberEmail;
+	private String mmDesc;
+	private Integer mmAdminNy;
+	private Integer mmDormacyNy;
+	private Integer mmTeacherNy;
+	private Integer mmSchoolCd;
+	private Integer mmGradeCd;
+	private Integer mmGenderCd;
+	private Integer mmTypeOfLoginCd;
+	private Integer mmTermsOfserviceNy;
+	private Integer mmEventNotificationNy;
+	private Integer mmPersonalInfoNy;
+	private Integer mmDelNy;
+	private Date mmPwdModDate;
+	private Date mmDob;
+	private Date regDateTime;
+	private Date regDateTimeSvr;
+	private Date modDateTime;
+	private Date modDateTimeSvr;
+	
+	public String getMmSeq() {
+		return mmSeq;
 	}
-
-
-	public void setIfmmSeq(String ifmmSeq) {
-		this.ifmmSeq = ifmmSeq;
+	public void setMmSeq(String mmSeq) {
+		this.mmSeq = mmSeq;
 	}
-
-	public String getIfmmId() {
-		return ifmmId;
+	public String getMmName() {
+		return mmName;
 	}
-
-	public void setIfmmId(String ifmmId) {
-		this.ifmmId = ifmmId;
+	public void setMmName(String mmName) {
+		this.mmName = mmName;
 	}
-
-
+	public String getMmId() {
+		return mmId;
+	}
+	public void setMmId(String mmId) {
+		this.mmId = mmId;
+	}
+	public String getMmPassword() {
+		return mmPassword;
+	}
+	public void setMmPassword(String mmPassword) {
+		this.mmPassword = mmPassword;
+	}
+	public String getMmMemberEmail() {
+		return mmMemberEmail;
+	}
+	public void setMmMemberEmail(String mmMemberEmail) {
+		this.mmMemberEmail = mmMemberEmail;
+	}
+	public String getMmDesc() {
+		return mmDesc;
+	}
+	public void setMmDesc(String mmDesc) {
+		this.mmDesc = mmDesc;
+	}
+	public Integer getMmAdminNy() {
+		return mmAdminNy;
+	}
+	public void setMmAdminNy(Integer mmAdminNy) {
+		this.mmAdminNy = mmAdminNy;
+	}
+	public Integer getMmDormacyNy() {
+		return mmDormacyNy;
+	}
+	public void setMmDormacyNy(Integer mmDormacyNy) {
+		this.mmDormacyNy = mmDormacyNy;
+	}
+	public Integer getMmTeacherNy() {
+		return mmTeacherNy;
+	}
+	public void setMmTeacherNy(Integer mmTeacherNy) {
+		this.mmTeacherNy = mmTeacherNy;
+	}
+	public Integer getMmSchoolCd() {
+		return mmSchoolCd;
+	}
+	public void setMmSchoolCd(Integer mmSchoolCd) {
+		this.mmSchoolCd = mmSchoolCd;
+	}
+	public Integer getMmGradeCd() {
+		return mmGradeCd;
+	}
+	public void setMmGradeCd(Integer mmGradeCd) {
+		this.mmGradeCd = mmGradeCd;
+	}
+	public Integer getMmGenderCd() {
+		return mmGenderCd;
+	}
+	public void setMmGenderCd(Integer mmGenderCd) {
+		this.mmGenderCd = mmGenderCd;
+	}
+	public Integer getMmTypeOfLoginCd() {
+		return mmTypeOfLoginCd;
+	}
+	public void setMmTypeOfLoginCd(Integer mmTypeOfLoginCd) {
+		this.mmTypeOfLoginCd = mmTypeOfLoginCd;
+	}
+	public Integer getMmTermsOfserviceNy() {
+		return mmTermsOfserviceNy;
+	}
+	public void setMmTermsOfserviceNy(Integer mmTermsOfserviceNy) {
+		this.mmTermsOfserviceNy = mmTermsOfserviceNy;
+	}
+	public Integer getMmEventNotificationNy() {
+		return mmEventNotificationNy;
+	}
+	public void setMmEventNotificationNy(Integer mmEventNotificationNy) {
+		this.mmEventNotificationNy = mmEventNotificationNy;
+	}
+	public Integer getMmPersonalInfoNy() {
+		return mmPersonalInfoNy;
+	}
+	public void setMmPersonalInfoNy(Integer mmPersonalInfoNy) {
+		this.mmPersonalInfoNy = mmPersonalInfoNy;
+	}
+	public Integer getMmDelNy() {
+		return mmDelNy;
+	}
+	public void setMmDelNy(Integer mmDelNy) {
+		this.mmDelNy = mmDelNy;
+	}
+	public Date getMmPwdModDate() {
+		return mmPwdModDate;
+	}
+	public void setMmPwdModDate(Date mmPwdModDate) {
+		this.mmPwdModDate = mmPwdModDate;
+	}
+	public Date getMmDob() {
+		return mmDob;
+	}
+	public void setMmDob(Date mmDob) {
+		this.mmDob = mmDob;
+	}
+	public Date getRegDateTime() {
+		return regDateTime;
+	}
+	public void setRegDateTime(Date regDateTime) {
+		this.regDateTime = regDateTime;
+	}
+	public Date getRegDateTimeSvr() {
+		return regDateTimeSvr;
+	}
+	public void setRegDateTimeSvr(Date regDateTimeSvr) {
+		this.regDateTimeSvr = regDateTimeSvr;
+	}
+	public Date getModDateTime() {
+		return modDateTime;
+	}
+	public void setModDateTime(Date modDateTime) {
+		this.modDateTime = modDateTime;
+	}
+	public Date getModDateTimeSvr() {
+		return modDateTimeSvr;
+	}
+	public void setModDateTimeSvr(Date modDateTimeSvr) {
+		this.modDateTimeSvr = modDateTimeSvr;
+	}
+	
+	
+	
+	
 
 }
