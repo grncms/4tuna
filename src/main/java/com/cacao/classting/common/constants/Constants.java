675c7327d18ee31f21132f6e8a85591097646c33
dbstnqls12 <binson1123@naver.com>
2022-05-15 12:26:26 +0900

    사집업로드를 위한 수정

--- src/main/java/com/cacao/classting/common/constants/Constants.java
+++ src/main/java/com/cacao/classting/common/constants/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
 	public static final String DATE_FORMAT_BASIC = "yyyy-MM-dd";
 	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
 	public static final int DATE_INTERVAL= -7;
-	
+	public static final String UPLOAD_PATH_PREFIX = "C:/factory/ws_sts_4130/4tuna/src/main/webapp/resources/uploaded/";
 }
