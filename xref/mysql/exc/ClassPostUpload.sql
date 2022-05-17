753076afda2b47b66c762ce99784fe2b54417d11
grncms <twino2@naver.com>
2022-05-17 09:27:46 +0900

    post

--- xref/mysql/exc/ClassPostUpload.sql
+++ xref/mysql/exc/ClassPostUpload.sql
@@ -1,20 +1,18 @@
-select ctptTitle, ctptContent,ctptWriter,ctptTag from ctPost where ctboSeq = 1;
+select ctptTitle, ctptContent,ctptWriter,regDateTime from ctPost where ctboSeq = 1;
 
 -- 등록  
-insert into ctPost (
-    ctptTitle,
-	ctptContent,
-    ctptWriter,
-    ctptTag
-    )
+insert into ctPost (ctptTitle, ctptContent, ctptWriter, ctptTag,regDateTime)
 select
 	ctptTitle,
 	ctptContent,
     ctptWriter,
-    ctptTag 
+    ctptTag,
+    regDateTime
 
     from ctPost
     where ctboSeq = 1;
 
+
+
+
 -- 수정
-update 
