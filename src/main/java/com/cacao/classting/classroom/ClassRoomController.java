a635b1372d69ab8767f99268db4b85e31925687f
dbstnqls12 <binson1123@naver.com>
2022-05-20 14:18:52 +0900

    컨트롤러 수정

--- src/main/java/com/cacao/classting/classroom/ClassRoomController.java
+++ src/main/java/com/cacao/classting/classroom/ClassRoomController.java
@@ -193,6 +193,12 @@ public class ClassRoomController {
 		httpSession.setAttribute("ctcmName", rt.getCtcmName());
 		httpSession.setAttribute("teacherNy", rt.getCtcmTeacherNy());
 		
+		vo.setCtcsYear((String) httpSession.getAttribute("ctcsYear"));
+		vo.setCtcsYear((String) httpSession.getAttribute("ctcsName"));
+		vo.setCtcsYear((String) httpSession.getAttribute("ctcmSeq"));
+		vo.setCtcsYear((String) httpSession.getAttribute("ctcmName"));
+		vo.setCtcsYear((String) httpSession.getAttribute("teacherNy"));
+		
 		System.out.println("httpSession.setAttribute(\"ctcsName\", rt.getCtcsName()) : " + rt.getCtcsName());
 		System.out.println("httpSession.setAttribute(\"ctcsName\", rt.getCtcsYear()) : " + rt.getCtcsYear());
 		System.out.println("httpSession.setAttribute(\"ctcmSeq\", rt.getCtcmSeq()) : " + rt.getCtcmSeq());
